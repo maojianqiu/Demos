3076765e455080ffb0cca99151965f68ec8a4d82
vae <13803345745@163.com>
2021-11-05 07:50:01 +0800

    添加springmvc视图

--- Demo4/spring-mvc/src/controller/IndexController.java
+++ Demo4/spring-mvc/src/controller/IndexController.java
@@ -9,7 +9,8 @@ import org.springframework.web.servlet.mvc.Controller;
 public class IndexController implements Controller {
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		System.out.println("-----------------");
-		ModelAndView mav = new ModelAndView("index.jsp");
+//		ModelAndView mav = new ModelAndView("index.jsp");
+		ModelAndView mav = new ModelAndView("index");
 		mav.addObject("message", "Hello Spring MVC");
 		return mav;
 	}
