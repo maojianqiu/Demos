64f29879cfd4340fc1b03111a40c8d0524087b33
mjq <13803345745@163.com>
2021-12-14 16:52:38 +0800

    联系mall

--- Project01/linjiashop-master/linjiashop-admin/src/api/dashboard.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-export  default{
-  get:function(){
-    return request({
-      url: '/dashboard',
-      method: 'get',
-    })
-  }
-}
--- Project01/linjiashop-master/linjiashop-mobile/src/utils/util.js
+++
@@ -1,5 +0,0 @@
-export default {
-    formatMobile: function (mobile) {
-        return mobile.substr(0, 3) + '****' + mobile.substr(7, 11);
-    }
-}
