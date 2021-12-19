6ec2d30b013181b20c55a2fa904959855bccc688
vae <13803345745@163.com>
2021-12-19 21:42:04 +0800

    -

--- springsecurity01/demo01/src/main/java/com/vae/demo01/config/WebSecurityConfig.java
+++ springsecurity01/demo01/src/main/java/com/vae/demo01/config/WebSecurityConfig.java
@@ -1,8 +1,13 @@
 package com.vae.demo01.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /*
 * 如果你使用的依赖包是依赖于 spring-boot-starter-parent 的，就只用 @Configuration 注解即可。
@@ -14,6 +19,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 * */
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    //-------------------------------------------------------------------------------------------------------
     // 3. 的代码
 //    @Override
 //    protected void configure(HttpSecurity http) throws Exception {
@@ -32,21 +38,31 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //
 //    }
 
-
+    //-------------------------------------------------------------------------------------------------------
     // 4. 的代码
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                //2.设置自定义登录页面和请求不设置访问权限
+                //设置自定义登录页面和请求不设置访问权限
                 .antMatchers("/newlogin.html").permitAll()
-                .antMatchers("/autho/admin/*").permitAll()
+                //设置游客、admin、user访问的权限，.hasRole(Str) 方法是给匹配的路径设置角色，当用户有这个角色时就可以访问这个路径。
+                .antMatchers("/autho/all/**").permitAll()
+                .antMatchers("/autho/admin/**").hasRole("ADMIN")
+                .antMatchers("/autho/user/**").hasRole("USER")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                //1.设置自定义的登录页面
+                //设置自定义的登录页面
                 .loginPage("/newlogin.html")
                 .and()
                 .csrf().disable();
+    }
 
+    @Bean
+    public UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("admin").password("123456").roles("ADMIN").build());
+        manager.createUser(User.withUsername("user").password("123456").roles("user").build());
+        return manager;
     }
 }
